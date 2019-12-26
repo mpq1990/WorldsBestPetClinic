c20011578a9a1ec47cbe8df8cc4773f9e4867e94
Majid Qureshi <mpq1990@gmail.com>
2019-12-26 13:56:37 +0100

    chore: add project initial files and source

---
+++ src/main/java/com/example/worldsbestbestclinic/WorldsBestBestClinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.worldsbestbestclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldsBestBestClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldsBestBestClinicApplication.class, args);
+    }
+
+}
