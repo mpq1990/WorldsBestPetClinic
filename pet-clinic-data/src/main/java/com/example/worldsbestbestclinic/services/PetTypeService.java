609d8a41a0711e3ca1ba42f25dbe6b5ae56bd2b8
Majid Qureshi <mpq1990@gmail.com>
2019-12-29 13:09:57 +0100

    feat: add pettypemap service. Closes #40

---
+++ pet-clinic-data/src/main/java/com/example/worldsbestbestclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.example.worldsbestbestclinic.services;
+
+import com.example.worldsbestbestclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
