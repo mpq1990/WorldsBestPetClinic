b94b66c00647e7081a12eb3f2718bc24ccefd90c
Majid Qureshi <mpq1990@gmail.com>
2019-12-29 13:41:34 +0100

    feat: add speciality map service. Closed #42

---
+++ pet-clinic-data/src/main/java/com/example/worldsbestbestclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.example.worldsbestbestclinic.services;
+
+import com.example.worldsbestbestclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
